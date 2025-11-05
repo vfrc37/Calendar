4321c629c0e48185810bf32ed7681a86f1dd8d9e
vfrc37 <maxim.ermishkin37@gmail.com>
2025-11-05 03:17:43 +0300

    feat(components): стили состояний для элементов календаря

--- calendar/src/components/Calendar/Calendar.vue
+++ calendar/src/components/Calendar/Calendar.vue
@@ -6,7 +6,7 @@
           <th v-for="label in calendar?.daysOrder" :key="label">{{ label }}</th>
         </tr>
         <tr v-for="week, i in calendar?.days" :key="i">
-          <th v-for="day, j in week" :key="j" @click="selectDate(day.day, day.month, day.year)">{{ day.day }}</th>
+          <th v-for="day, j in week" :key="j" :class="{ 'current-date': checkCurrentDay(day), 'current-month': checkCurrentMonth(day), date: true }" @click="selectDate(day.day, day.month, day.year)">{{ day.day }}</th>
         </tr>
       </tbody>
     </table>
@@ -16,6 +16,7 @@
 <script lang="ts">
 
 import { CalendarModel } from './CalendarModel';
+import type { TCalendarDate } from './types/Calendar';
 
 export default {
   name: 'CalendarWidget',
@@ -57,7 +58,16 @@ export default {
       if (!this.calendar) return;
       const selectedValue = `${year}-${month + 1}-${day}`;
       this.updateCalendar(selectedValue);
-    }
+    },
+    checkCurrentMonth(day: TCalendarDate) {
+      if (!this.selectedDate) return false;
+      return this.calendar?.currentMonth === day.month;
+    },
+    checkCurrentDay(day: TCalendarDate) {
+      if (!this.selectedDate) return false;
+      const selDate = new Date(this.selectedDate)
+      return selDate.getDate() === day.day && selDate.getMonth() === day.month && selDate.getFullYear() === day.year;
+    },
   },
   created() {
     // первая инициализация. если дата не передана, используется текущая дата
@@ -73,6 +83,10 @@ export default {
 </script>
 
 <style scoped lang="scss">
+
+  $color-active: blue;
+  $color-inactive: grey;
+
   .wrapper {
     border: 1px solid rgb(185, 185, 185);
     padding: 5px;
@@ -81,9 +95,23 @@ export default {
   th {
     width: 30px;
     height: 30px;
+    border: 1px solid transparent;
+
+    &.current-date {
+      border-color: $color-active;
+    }
 
     &.date {
       font-size: 1.1em;
+
+      &:hover {
+        border-color: $color-active;
+        cursor: pointer;
+      }
+    }
+
+    &:not(.current-month) {
+      color: $color-inactive;
     }
   }
 </style>
